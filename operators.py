edb569523e9244d98a576cedc8232725f88f8c57
Harshitha reddy <pannalaharshithareddy@gmail.com>
2025-07-22 14:31:23 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,33 @@
+#exponential
+a=5**2
+print(a)
+#finding sqrt
+a=25**0.5
+print(a)
+#assignment
+
+
+#bitwise
+a=32
+b=18
+c=a&b
+d=a^c
+e=d|b
+print(e)
+
+#leftshift
+print(20<<1)
+
+
+#identity operator
+a,b=10,10
+print(a is b)
+c=20
+print(a is not c)
+
+#membership operators
+a=(1,2,3,4)
+print(3 in a)
+print(5 in a)
+print(7 not in a)
+ 
