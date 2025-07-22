1ad5add9d84d29028b584c92a8385974467a3226
Harshitha reddy <pannalaharshithareddy@gmail.com>
2025-07-22 14:31:37 +0530

    Add files via upload

---
+++ outputfunction.py
@@ -0,0 +1,21 @@
+print("string",sep=' ',end='/')
+print("vits" ,"CSE", sep='@',end='?')
+print("Good day")
+
+#simple interest
+
+P=10000
+T=12
+R=1.5
+print((P*R*T)/100)
+
+#pi*r^2
+
+r=5
+pi=3.14
+print((pi*(r*r)))
+
+#square
+
+sq=4
+print(sq*sq)
