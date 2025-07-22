62290b374901055c7643580b82d3476ba1af5d67
Harshitha reddy <pannalaharshithareddy@gmail.com>
2025-07-22 14:30:17 +0530

    Add files via upload

---
+++ Firstfile.py
@@ -0,0 +1,23 @@
+#first file
+a=10
+print(a,type(a))
+a=10.5
+print(a,type(a))
+a=3+5j
+print(a,type(a))
+a=True
+print(a,type(a))
+a=False
+print(a,type(a))
+a=None
+print(a,type(a))
+a=[1,2]
+print(a,type(a))
+a={1,2}
+print(a,type(a))
+a=(1,2)
+print(a,type(a))
+a={'x':10}
+print(a,type(a))
+a="string"
+print(a,type(a))
